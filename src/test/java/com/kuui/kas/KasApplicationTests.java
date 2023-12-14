f949e920a5a1302c361a5e1f5e59cc1c6f55ca5e
yhn032 <87313203+yhn032@users.noreply.github.com>
2023-12-14 17:52:59 +0900

    first Setting Commit

---
+++ src/test/java/com/kuui/kas/KasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuui.kas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
