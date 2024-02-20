69a0468da2696f708e4c8d98c9606b12cf6f3338
yhn032 <87313203+yhn032@users.noreply.github.com>
2024-02-20 14:44:20 +0900

    메인 페이지 디자인 수정 -> 개인 블로그 디자인 사이드바 추가 모든 페이지 공통 관리 navBar에 서브 메뉴 추가 스크롤바 수정 스크롤시 헤더 따라오게 만들어야 함

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,53 @@
+const colors = ['#ff0000', '#00ff00', '#0000ff'];
+const header = document.querySelector(".content-navBar");
+let idx = 0;
+
+//일정 시간마다 배경색 변경 함수
+/*function changeBackground(){
+    header.style.backgroundColor = colors[idx];
+    idx = (idx+1) % colors.length;
+}
+//3초를 주기로 메서드 실행
+setInterval(changeBackground, 3000);*/
+
+function toggleSidebar() {
+    if($(".sidebar").css("margin-left") === '-250px'){ //안보이는 경우
+        $(".sidebar").css("margin-left", '0');
+        $(".content").css("margin-left", '250px');
+        $(".asset-main").css("margin-left", '250px');
+        $(".content-navBar").css("margin-left", '250px');
+        $(".content-footer").css("margin-left", '250px');
+        $(".content-toggle-btn").text("◀");
+        $(".content-toggle-btn").css("left", '261px');
+    } else {
+        $(".sidebar").css("margin-left", '-250px');
+        $(".content").css("margin-left", '0');
+        $(".asset-main").css("margin-left", '0');
+        $(".content-navBar").css("margin-left", '0');
+        $(".content-footer").css("margin-left", '0');
+        $(".content-toggle-btn").text("▶");
+        $(".content-toggle-btn").css("left", '11px');
+    }
+}
+
+let lastScrollTop = 0;
+window.addEventListener('scroll', function(event){
+    const contentToggleBtn = document.querySelector('.content-toggle-btn');
+    const computedStyle = window.getComputedStyle(contentToggleBtn);
+
+    const lastValue = computedStyle.getPropertyValue('bottom').replace('px', '');
+
+    let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop) {
+        contentToggleBtn.style.bottom = parseInt(lastValue)-(currentScrollTop-lastScrollTop)+'px';
+        console.log(currentScrollTop-lastScrollTop);
+        /*contentToggleBtn.style.bottom =*/
+    } else {
+        contentToggleBtn.style.bottom = parseInt(lastValue)+(lastScrollTop-currentScrollTop)+'px';
+        console.log(lastScrollTop-currentScrollTop);
+    }
+
+    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+
+});
---
+++ src/main/resources/static/js/navBar.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    const prevScrollPos = $(window).scrollTop();
+    console.log("prevScrollPos :: " + prevScrollPos);
+
+    window.addEventListener('scroll', function(event){
+        const currentScrollPos = $(window).scrollTop();
+        const navBar = $(".content-navBar");
+
+        console.log("currentScrollPos :: " + currentScrollPos);
+        console.log("prevScrollPos :: " + prevScrollPos);
+
+        if(prevScrollPos > currentScrollPos) {
+            alert("hide");
+        }else {
+            alert("show");
+        }
+    });
+
+    $('.has-asset-sub-menu > a').click(function (e){
+        e.preventDefault();
+        $('.arrow-up').toggle();
+        $('.asset-sub-menu').toggle();
+    });
+});
